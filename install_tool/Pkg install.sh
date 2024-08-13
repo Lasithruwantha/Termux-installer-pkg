12e0c5e60d6fdf6b78fd4dd7ef8b568e2a4a9b61
Lasithruwantha <lasithruwanthalasith@gmail.com>
2024-08-13 13:31:13 +0530

    Add files via upload

---
+++ install_tool/Pkg install.sh
@@ -0,0 +1,14 @@
+printf "All pkg install"
+sleep 0.9
+pkg install nano
+printf "\033[0;31nano"
+pkg install mkdir
+printf "\033[0:31mmkdir"
+pkg install php
+printf "\033[0;31mphp"
+pkg install ls 
+printf "\033[0;31mls"
+pkg install python 
+pkg install python2
+printf "\033[0;31mpython2 and python1"
+printf "\033[0;34m all pkg done\n"
